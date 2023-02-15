45d5bd6e6e2a75a6ed051da1fc2cae607c25e200
Junjie_Li <jfjunjieli@gmail.com>
2023-02-15 01:54:21 +0100

    vue example

---
+++ vue/vue-example/component/simple-component.vue
@@ -0,0 +1,41 @@
+<!-- https://cn.vuejs.org/examples/#simple-component -->
+
+<!--
+这里展示了最简单的组件，它接收一个 prop 并渲染出来。
+在指南页面了解更多关于组件的内容！
+-->
+
+<script>
+import TodoItem from "./TodoItem.vue";
+
+export default {
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      groceryList: [
+        { id: 0, text: "Vegetables" },
+        { id: 1, text: "Cheese" },
+        { id: 2, text: "Whatever else humans are supposed to eat" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <ol>
+    <!--
+      我们给每个 todo 项提供它所表示的 todo 对象，
+      以便能够动态展示内容。
+      同时还需要给每个组件提供一个“key”，
+      这在指南的 v-for 部分有详细解释。
+    -->
+    <TodoItem
+      v-for="item in groceryList"
+      :todo="item"
+      :key="item.id"
+    ></TodoItem>
+  </ol>
+</template>
---
+++ vue/vue-example/hello-world.vue
@@ -0,0 +1,15 @@
+<!-- https://cn.vuejs.org/examples/#hello-world -->
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
