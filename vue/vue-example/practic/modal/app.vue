fbd21f47168805a10afbdac0535e53417606bdc5
Junjie_Li <jfjunjieli@gmail.com>
2023-02-16 02:08:39 +0100

    vue practic example

---
+++ vue/vue-example/practic/modal/app.vue
@@ -0,0 +1,33 @@
+<!-- https://cn.vuejs.org/examples/#modal -->
+
+<!--
+可定制插槽和 CSS 过渡效果的模态框组件。
+-->
+
+<script>
+import Modal from "./Modal.vue";
+
+export default {
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <!-- 使用这个 modal 组件，传入 prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </modal>
+  </Teleport>
+</template>
