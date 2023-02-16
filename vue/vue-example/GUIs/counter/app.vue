f926d9d8b244357647934328c0efe1eccacae4a6
Junjie_Li <jfjunjieli@gmail.com>
2023-02-16 02:14:14 +0100

    vue guis example

---
+++ vue/vue-example/GUIs/counter/app.vue
@@ -0,0 +1,20 @@
+<!-- https://cn.vuejs.org/examples/#counter -->
+
+<!--
+https://eugenkiss.github.io/7guis/tasks/#counter
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  {{ count }}
+  <button @click="count++">Count</button>
+</template>
