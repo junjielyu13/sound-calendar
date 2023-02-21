0bcee265451ccd205d14fae0b2d77fc6b92a227a
Junjie_Li <jfjunjieli@gmail.com>
2023-02-21 13:19:15 +0100

    refactory: prettierrc

--- sound-calendar/src/components/HelloWorld.vue
+++ sound-calendar/src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script lang="ts">
+export default {
+  data() {
+    return {
+      msg: "Hello World!",
+    };
+  },
+};
 </script>
 
 <template>
@@ -10,7 +14,8 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      What's next?
     </h3>
   </div>
 </template>
